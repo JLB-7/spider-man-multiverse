48f3f56868d5aad26dde6994360281fff335e483
JLB-7 <luisbarauna25@gmail.com>
2024-12-14 23:36:21 -0300

    first commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,38 @@
+function handleMouseEnter() {
+  this.classList.add('card--hovered');
+  document.body.id = `${this.id}-hovered`;
+}
+
+function handleMouseLeave() {
+  this.classList.remove('card--hovered');
+  document.body.id = '';
+}
+
+function addEventListenersToCards() {
+  const cardsElements = document.getElementsByClassName('card');
+  
+  for (let i = 0; i < cardsElements.length; i++) {
+    const card = cardsElements[i];
+    card.addEventListener('mouseenter', handleMouseEnter);
+    card.addEventListener('mouseleave', handleMouseLeave);
+  }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', addEventListenersToCards, false)
+
+function selectCarouselItem(selectedButtonElement) {
+  const selectedItem = selectedButtonElement.id;
+  const carousel = document.querySelector('.cards-carousel');
+  const transform = carousel.style.transform;
+  const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+  const rotateYDeg = -120 * (Number(selectedItem) - 1);
+  const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+
+  carousel.style.transform = newTransform;
+
+  const activeButtonElement = document.querySelector('.controller-button--active');
+  activeButtonElement.classList.remove('controller-button--active');
+  selectedButtonElement.classList.add('controller-button--active');
+}
